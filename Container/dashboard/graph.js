acb5c3543895fd9cb88ade203678fbce4f89c48e
sribs <sribs@microsoft.com>
2019-07-24 19:23:38 +0530

    commiting Final Changes

--- Container/dashboard/graph.js
+++ Container/dashboard/graph.js
@@ -1,75 +1,138 @@
+var sensor_api="http://agrosmartservice.southindia.cloudapp.azure.com:9000/api/device/";
+var user_api = "http://agrosmartservice.southindia.cloudapp.azure.com:9000/api/user/";
+var status_api = "http://agrosmartservice.southindia.cloudapp.azure.com:9000/api/devicestatus/";
+var rain_percent = {"moderate rain":"50%", "heavy rain":"75%", "light rain":"25%"};
+var mac_address = "";
+
 function rand() {
     return Math.random();
-  }
-  
-  var time = new Date();
-  
-  var data1 = [{
-    x: [time],
-    y: 25,
-    mode: 'lines',
-    line: {color: '#00A8A9'}
-  }]
+}
 
-  var data2 = [{
-    x: [time],
-    y: 61,
-    mode: 'lines',
-    line: {color: '#00A8A9'}
-  }]
+function populateUserData() {
+    var username = document.getElementById("username").value;
+    console.log(username)
+    $.getJSON(user_api+username,function(data){
+        console.log(username)
+        var select = document.getElementById("selectmac"); 
+        var options = data["deviceList"]; 
+    
+        for(var i = 0; i < options.length; i++) {
+            var opt = options[i];
+            var el = document.createElement("option");
+            el.textContent = opt;
+            el.value = opt;
+            select.appendChild(el);
+        }
 
-  var data3 = [{
-    x: [time],
-    y: 1,
-    mode: 'lines',
-    line: {color: '#00A8A9'}
-  }]
-  
-  var layout = {
-      title: 'Temperature Sensor',
-      xaxis: {
-        title: 'Timestamp'
-      },
-      yaxis: {
-        title: 'Temperature'
-      }
-  };
+        data = data["forecastData"]
+        document.getElementById("weather").innerHTML = data["list"][2]["weather"][0]["description"]
+        document.getElementById("temp").innerHTML = data["list"][2]["main"]["temp"]
+        document.getElementById("humidity").innerHTML = data["list"][2]["main"]["humidity"].toString()+"%"
+        document.getElementById("rain").innerHTML = rain_percent[data["list"][2]["weather"][0]["description"]]
+    });
+}
+var time = new Date();
 
-  var layout2 = {
-    title: 'Humidity Sensor',
-    xaxis: {
-        title: 'Timestamp'
-      },
-      yaxis: {
-        title: 'Humidity'
-      }
-  };
+function initplot()
+{
+    $.getJSON(sensor_api, function(data){
+        var data1 = [{
+            x: [time],
+            y: [data["temperature"]],
+            mode: 'lines',
+            line: {color: '#00A8A9'}
+        }]
 
-  var layout3 = {
-    title: 'Moisture Sensor',
-    xaxis: {
-        title: 'Timestamp'
-      },
-      yaxis: {
-        title: 'Moisture'
-    }
-  };
-  
-  Plotly.newPlot('grapht', data1, layout);
-  Plotly.newPlot('graph2', data2, layout2);
-  Plotly.newPlot('graph3', data3, layout3);
-  
-  var cnt = 0;
-  
-  var interval = setInterval(function() {
-  
+        var data2 = [{
+            x: [time],
+            y: [data["humidity"]],
+            mode: 'lines',
+            line: {color: '#00A8A9'}
+        }]
+
+        var data3 = [{
+            x: [time],
+            y: [data["moisture"]],
+            mode: 'lines',
+            line: {color: '#00A8A9'}
+        }]
+        
+        var layout = {
+            title: 'Temperature Sensor',
+            xaxis: {
+                title: 'Timestamp'
+            },
+            yaxis: {
+                title: 'Temperature'
+            }
+        };
+
+        var layout2 = {
+            title: 'Humidity Sensor',
+            xaxis: {
+                title: 'Timestamp'
+            },
+            yaxis: {
+                title: 'Humidity'
+            }
+        };
+
+        var layout3 = {
+            title: 'Moisture Sensor',
+            xaxis: {
+                title: 'Timestamp'
+            },
+            yaxis: {
+                title: 'Moisture'
+            }
+        };
+        
+        Plotly.newPlot('grapht', data1, layout);
+        Plotly.newPlot('graph2', data2, layout2);
+        Plotly.newPlot('graph3', data3, layout3);
+    });
+}
+
+
+function updatePlot() {
+    var cnt=0;
+    var interval = setInterval(function() {
+    
     var time = new Date();
-  
-    $.getJSON("http://localhost:5000/SensorData")
-  
-    Plotly.extendTraces('grapht', update, [0])
-    Plotly.extendTraces('graph2', update, [0])
-    Plotly.extendTraces('graph3', update, [0])
-  
+    
+    $.getJSON(sensor_api, function(data){
+        var update_t = {
+            x:  [[time]],
+            y: [[data["temperature"]]]
+            };
+        var update_h = {
+            x:  [[time]],
+            y: [[data["humidity"]]]
+            };     
+        var update_m = {
+            x:  [[time]],
+            y: [[data["moisture"]]]
+            };     
+        Plotly.extendTraces('grapht', update_t, [0])
+        Plotly.extendTraces('graph2', update_h, [0])
+        Plotly.extendTraces('graph3', update_m, [0])
+    });
     if(cnt === 100) clearInterval(interval);
-  }, 3000);
+    }, 20000);
+}
+
+var stat_interval = setInterval(function() {
+    $.getJSON(status_api, function(data) {
+        document.getElementById('status').innerHTML = data["status"]
+    });
+}, 10000);
+
+
+function apiDisplay() {
+    mac_address = document.getElementById("selectmac").value;
+    sensor_api = sensor_api + mac_address
+    status_api = status_api + mac_address
+    document.getElementById('display').innerHTML = mac_address
+    initplot();
+    updatePlot();
+}
--- Container/web/sensors.py
+++ Container/web/sensors.py
@@ -1,11 +1,26 @@
 from flask import jsonify, request
 from flask_restful import Resource
+from mongo import db
 
-current_data = {"temperature":27, "humidity": 76, "moisture": 25}
+sensor = db["Sensors"]
 
 class SensorData(Resource):
     def post(self):
-        current_data = request.get_json()
+        data = request.get_json()
+        if sensor.find({'_id':data["_id"]}).count()==0:
+            sensor.insert_one(data)
+        else:
+            sensor.find_one_and_update({'_id':data["_id"]},{'$set':{'temperature':data["temperature"], 'humidity':data["humidity"], 'moisture':data["moisture"]}})
+        return jsonify({
+            'status':200,
+            'message': 'users adding successful'
+        })
     
-    def get(self):
-        return jsonify(current_data)
+    def get(self, mac_addr=None):
+        try:
+            if mac_addr is None:
+                return [data for data in sensor.find()]
+            return sensor.find_one({'_id':mac_addr})
+        except:
+            return dict()
+        
